8cfb38cb533df0839a377a7b4638456f311e2c73
yeee6661 <nobi74780@gmail.com>
2025-04-23 03:00:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,22 @@ btn.addEventListener("click", function () {
     window.location.href = "output.html";
 })
 
-fetch('netlify/edge-functions/loc')
-    .then(res => res.json())
-    .then(data => {
-        console.log("User IP:", data.ip);
+// This function will be triggered when the page is loaded
+window.addEventListener('load', async () => {
+  try {
+    // Make a request to the Edge Function hosted on your Netlify site
+    const response = await fetch('https://surprise-yourself.netlify.app/.netlify/edge-functions/loc', {
+      method: 'GET', // or POST depending on your Edge Function setup
     });
+
+    // Check if the request was successful
+    if (response.ok) {
+      const data = await response.json();
+      console.log('IP sent to Google Sheets:', data);
+    } else {
+      console.error('Error sending IP to Google Sheets');
+    }
+  } catch (error) {
+    console.error('Error fetching the Edge Function:', error);
+  }
+});
