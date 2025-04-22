2fbd0a56b49cb8aaeab220f47ecb33de42d29932
yeee6661 <nobi74780@gmail.com>
2025-04-23 02:17:41 +0530

    Update loc.js

--- netlify/edge-functions/loc/loc.js
+++ netlify/edge-functions/loc/loc.js
@@ -1,22 +1,19 @@
-
-exports.handler = async (event) => {
-  const ip = event.headers["x-forwarded-for"] || "Unknown";
-  const userAgent = event.headers["user-agent"];
+export default async (request, context) => {
+  const ip = request.headers.get("x-forwarded-for") || "Unknown";
+  const userAgent = request.headers.get("user-agent");
   const time = new Date().toISOString();
 
-  const sheetUrl = "https://sheet.best/api/sheets/e2WqMy7p21#WjjhIWJX_QSHFN6fiSLXr6N@byhbAO#sGN3ee9-txJO9!CVN2G7SW";
+  const sheetUrl = "https://sheet.best/api/sheets/e2WqMy7p21#WjjhIWJX_QSHFN6fiSLXr6N@byhbAO#sGN3ee9-txJO9!CVN2G7SW"; 
 
-
- 
   await fetch(sheetUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ time, ip, userAgent }),
   });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "IP logged" }),
-  };
+  return new Response(JSON.stringify({ message: "IP logged" }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 };
 
