e972740fc16a6aa316a6768bd20c2afd792e1c55
yeee6661 <nobi74780@gmail.com>
2025-04-23 04:45:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const text = document.querySelector("#main");
+const btn = document.querySelector(".btn");
+const change = document.querySelector(".here")
+btn.addEventListener("click", function () {
+    const name = text.value;
+    sessionStorage.setItem("userName", name);
+    window.location.href = "output.html";
+})
 window.addEventListener('load', () => {
   if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(
